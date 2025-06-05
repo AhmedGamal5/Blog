dc0fd24fa9f4fac23e80e63ee6e9f89bfc082341
ahmedgamal5 <ahmedgamalflame5534@gmail.com>
2025-06-05 16:06:20 +0300

    Fix API base URL by removing trailing slash

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
   // baseURL: 'http://localhost:3000',
-  baseURL: 'https://blog-back-end-mauve.vercel.app/',
+  baseURL: 'https://blog-back-end-mauve.vercel.app',
 });
 
 
